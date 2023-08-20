65da7d790582a3e3c60176048859859587c3880f
Artur Mavrodii <artmavrodiy@gmail.com>
2023-08-20 20:20:04 +0300

    first code line second attempt

--- src/main/java/ua/javarush/encoder/Application.java
+++ src/main/java/ua/javarush/encoder/Application.java
@@ -2,8 +2,6 @@ package ua.javarush.encoder;
 
 public class Application {
     public static void main(String[] args) {
-
-
-        System.out.println("Hi");
+        System.out.println("I'm starting to develope my first app soon");
     }
 }
